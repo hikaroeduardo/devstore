7190d775aaeb4bf1647daf3dafa6669a29bb0d82
Hikaro <hikaroeduardobandeira@gmail.com>
2024-01-23 17:05:02 -0300

    style: Estilizacao de layout e header

---
+++ src/app/(store)/search/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+    return <p>Carregando Pesquisa...</p>
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="pt-br">
-            <body className={inter.variable}>{children}</body>
+        <html className={inter.variable} lang="pt-br">
+            <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
         </html>
     );
 }
